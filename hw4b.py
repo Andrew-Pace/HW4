72ce3f351d5d45665fddc7eb83b7b54ce6bee30e
Andrew-Pace <158859241+Andrew-Pace@users.noreply.github.com>
2024-02-26 01:27:31 -0600

    Add files via upload

---
+++ hw4b.py
@@ -0,0 +1,81 @@
+
+import numpy as np
+from scipy.optimize import fsolve
+
+
+def func(x):
+    """
+        defines Function #1
+    :param x: x value to be evaluated
+    :return: Function 1 evaluated at x
+    """
+    f = x - 3*np.cos(x)
+    return f
+
+
+def func2(x):
+    """
+    defines Function #2
+    :param x: x value to be evaluated
+    :return: Function 2 evaluated at x
+    """
+    f2 = np.cos(2*x)*x**3
+    return f2
+
+
+def findIntersection(x0):
+    """
+    Uses fsolve to find the intersection point
+    :param x0: initial guess, can put np array into it
+    :return: root closest to the initial guess
+    """
+    return fsolve(lambda x : func(x) - func2(x),x0)  # find when func1 - func2 = 0, or func1 = func2
+
+
+def solver(arr, fcn):
+    """
+    Solves for the roots of the function, 
+    :param arr: the x values to be tested
+    :param fcn: the function to be called to return the equation
+    :return: roots of the equation in an array
+    """
+
+    roots = fsolve(fcn, arr)  # get roots using the arr passing into this function
+    newroots = np.unique(np.round(roots, 3))  # round and strip the duplicates of the roots
+
+    return newroots
+
+
+def main():
+    """
+    Main function, finds roots and intersects of 2 equations
+    :return: prints results to the CLI
+    """
+
+    min = -4  # min for guesses
+    max = 4  # max for guesses
+    arr = np.array([range(min, max)])  # create arr containing the guesses at each integer x value
+    print(f"Between x = {min} and x = {max}")  # print the bounds of our guesses
+
+    roots1=solver(arr, func)  # use solver to get roots of func1
+    print(f"Roots of Func1: ")
+    for i in range(roots1.size):  # prints the roots nicely
+        print(f"x={roots1[i]}")
+    print()
+
+    roots2=solver(arr, func2)  # use solver to get roots of func2
+    print(f"Roots of Func2:")
+    for i in range(roots2.size):  # prints the roots nicely
+        print(f"x={roots2[i]}")
+    print()
+
+    newresult = np.unique(np.round(findIntersection(arr), 3))  # get intersects, round and strip duplicates
+    print(f"Intersects between the 2 functions:")
+    for i in range(newresult.size):  # prints the intersects nicely
+        print(f"x={newresult[i]}")
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ hw4c.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+def solver(a, b):
+    """
+    Solves the system of equations given by a and b in the form of matrices
+    :param a: matrix of coefficients
+    :param b: matrix of answers for the system
+    :return: the solutions of the system in an array
+    """
+    results = np.linalg.solve(a, b)  # using numpy function to solve system
+    return results
+
+
+def main():
+    """
+    Solves the system of equations, has options for 2x2-5x5 matrices
+    :return: Prints the solutions to the CLI
+    """
+
+    # 2x2 Matrices, True to use, False to not use.
+    two = False
+    if two is True:
+        a2 = np.array([[4, 2], [5, 2]])  # defining matrix a
+        b2 = np.array([14, 16])  # defining matrix b
+        xtwo = np.round(solver(a2, b2), 3)  # solving for x1,x2,...
+        num_rows, num_cols = a2.shape  # getting measurements
+        for i in range(num_rows):  # for each row print the first equation from this system
+            print(f"({a2[i][0]})•x1 + ({a2[i][1]})•x2 = {b2[i]}")
+        for i in range(xtwo.size):  # prints the x values nicely
+            print(f"x{i + 1}={xtwo[i]}")
+        print()  # space for prettiness
+
+    # 3x3 Matrices, True to use, False to not use.
+    thr = True
+    if thr is True:
+        a3 = np.array([[3, 1, -1], [1, 4, 1], [2, 1, 2]])  # defining matrix a
+        b3 = np.array([2, 12, 10])  # defining matrix b
+        xthr = np.round(solver(a3, b3), 3)  # solving for x1,x2,...
+        num_rows, num_cols = a3.shape  # getting measurements
+        for i in range(num_rows):  # for each row print the first equation from this system
+            print(f"({a3[i][0]})•x1 + ({a3[i][1]})•x2 + ({a3[i][2]})•x3 = {b3[i]}")
+        for i in range(xthr.size):  # prints the x values nicely
+            print(f"x{i + 1}={xthr[i]}")
+        print()  # space for prettiness
+
+    # 4x4 Matrices, True to use, False to not use.
+    four = True
+    if four is True:
+        a4 = np.array([[1, -10, 2, 4], [3, 1, 4, 12], [9, 2, 3, 4], [-1, 2, 7, 3]])  # defining matrix a
+        b4 = np.array([2, 12, 21, 37])  # defining matrix b
+        xfour = np.round(solver(a4, b4), 3)  # solving for x1,x2,...
+        num_rows, num_cols = a4.shape  # getting measurements
+        for i in range(num_rows):  # for each row print the first equation from this system
+            print(f"({a4[i][0]})•x1 + ({a4[i][1]})•x2 + ({a4[i][2]})•x3 + ({a4[i][3]})•x4 = {b4[i]}")
+        for i in range(xfour.size):  # prints the roots nicely
+            print(f"x{i + 1}={xfour[i]}")
+        print()  # space for prettiness
+
+    five = False
+    if five is True:
+        # defining matrix a
+        a5 = np.array([[1, -10, 2, 4, 3], [3, 1, 4, 12, 7], [9, 2, 3, 4, 1], [-1, 2, 7, 3, 3], [4, 2, 7, 4, 2]])
+        b5 = np.array([2, 12, 21, 37, 32])  # defining matrix b
+        xfive = np.round(solver(a5, b5), 3)  # solving for x1,x2,...
+        num_rows, num_cols = a5.shape  # getting measurements
+        for i in range(num_rows):  # for each row print the first equation from this system
+            print(f"({a5[i][0]})•x1 + ({a5[i][1]})•x2 + ({a5[i][2]})•x3 + ({a5[i][3]})•x4 + ({a5[i][4]})•x5 = {b5[i]}")
+        for i in range(xfive.size):  # prints the roots nicely
+            print(f"x{i + 1}={xfive[i]}")
+        print()  # space for prettiness
+
+
+if __name__ == '__main__':
+    main()
+
+# couldn't figure out how to print the entire system nicely without just making separate entries for different size
+# matrices.
